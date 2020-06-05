072a4601dabb294041e736aa7c5b9052c08aeead
Danilo Petruce <danilopetruce@hotmail.com>
2020-06-05 17:12:22 -0300

    Ajustes finais - Projeto finalizado

--- src/server.js
+++ src/server.js
@@ -1,42 +1,32 @@
 const express = require("express");
 const server = express();
 
-// pegar o banco de dados
+// Pegar o banco de dados
 const db = require("./database/db");
 
-// configurar pasta publica
+// Configurar pasta publica
 server.use(express.static("public"));
 
-// habilitar o uso do req.body na nossa aplicação
+// Habilitar o uso do req.body na nossa aplicação
 server.use(express.urlencoded({ extended: true }));
 
-// utilizando template engine
+// Utilizando template engine
 const nunjucks = require("nunjucks");
 nunjucks.configure("src/views", {
   express: server,
   noCache: true,
 });
 
-// configurar caminhos da minha aplicação
-// página inicial
-// req: Requisição
-// res: Resposta
 server.get("/", (req, res) => {
   return res.render("index.html", { title: "Um título" });
 });
 
 server.get("/create-point", (req, res) => {
-  // req.query: Query Strings da nossa url
-  // console.log(req.query)
-
   return res.render("create-point.html");
 });
 
 server.post("/savepoint", (req, res) => {
-  // req.body: O corpo do nosso formulário
-  // console.log(req.body)
-
-  // inserir dados no banco de dados
+  // Inserir dados no banco de dados
   const query = `
         INSERT INTO places (
             image,
@@ -78,11 +68,11 @@ server.get("/search", (req, res) => {
   const search = req.query.search;
 
   if (search == "") {
-    // pesquisa vazia
+    // Pesquisa vazia
     return res.render("search-results.html", { total: 0 });
   }
 
-  // pegar os dados do banco de dados
+  // Pegar os dados do banco de dados
   db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (
     err,
     rows
@@ -93,10 +83,9 @@ server.get("/search", (req, res) => {
 
     const total = rows.length;
 
-    // mostrar a página html com os dados do banco de dados
+    // Mostrar a página html com os dados do banco
     return res.render("search-results.html", { places: rows, total: total });
   });
 });
 
-// ligar o servidor
 server.listen(3333);
